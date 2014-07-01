2963850d6323920eb1f5b90b575d5ded85252e03
Fahim <olein1992@yahoo.com>
2014-07-01 12:01:07 +0600

    CRUD version 2. Account.java is improved

--- src/fahim/Account.java
+++ src/fahim/Account.java
@@ -2,7 +2,11 @@ package fahim;
 
 import java.sql.*;
 import java.util.Map;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.opensymphony.xwork2.ActionContext;
 
@@ -15,9 +19,8 @@ public class Account {
 	private String Newpassword;
 	private String Result = " ";
 
-	public String Login() {
-		System.out.println("MySQL Connect Example.");
-		Connection conn = null;
+	public Connection DBconnection(Connection conn) {
+
 		String url = "jdbc:mysql://localhost:3306/";
 		String dbName = "USER";
 		String driver = "com.mysql.jdbc.Driver";
@@ -29,194 +32,152 @@ public class Account {
 					password1);
 			System.out.println("Connected to the database");
 
-			Statement stm = conn.createStatement();
-			ResultSet rs0 = stm
-					.executeQuery("SELECT * FROM ACCOUNT WHERE name='" + name
-							+ "' AND password='" + password + "';");
-
-			if (rs0.next() == true) {
-				conn.close();
-				System.out.println("Disconnected from database");
-				return "success";
-			} else {
-				return "failure";
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.print("failed\n");
 		}
+		return conn;
 
+	}
+
+	public int Select(Connection conn, String name, String password) {
+		int id = 0;
+		try {
+			PreparedStatement preparedStatement1 = conn
+					.prepareStatement("select * from ACCOUNT where name=? and password=?");
+			preparedStatement1.setString(1, name);
+			preparedStatement1.setString(2, password);
+			ResultSet rs = preparedStatement1.executeQuery();
+			if(rs.next())
+			{
+				id = rs.getInt("id");
+			}
+			return id;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
+
+	public String Login() throws SQLException {
+		System.out.println("MySQL Connect Example.");
+		Connection conn = null;
+
+		conn = DBconnection(conn);
+
+		Statement stm = conn.createStatement();
+		ResultSet rs0 = stm.executeQuery("SELECT * FROM ACCOUNT WHERE name='"
+				+ name + "' AND password='" + password + "';");
+
+		if (rs0.next() == true) {
+			conn.close();
+			System.out.println("Disconnected from database");
+			return "success";
+		} else {
+			return "failure";
+		}
+	}
+
+	public String Logout() {
+		Map session = ActionContext.getContext().getSession();
+		session.remove("logged-in");
 		return "success";
 	}
-	
-	public String Logout()
-	{
-		  Map session = ActionContext.getContext().getSession();
-		  session.remove("logged-in"); 
-		  return "success";
-	}
 
-	public String Create() {
+	public String Create() throws SQLException {
 		if (password.equals(passwordConfirm)) {
 
 			System.out.println("MySQL Connect Example.");
 			Connection conn = null;
-			String url = "jdbc:mysql://localhost:3306/";
-			String dbName = "USER";
-			String driver = "com.mysql.jdbc.Driver";
-			String userName = "root";
-			String password1 = "";
+			conn = DBconnection(conn);
+
 			try {
-				Class.forName(driver).newInstance();
-				conn = DriverManager.getConnection(url + dbName, userName,
-						password1);
-				System.out.println("Connected to the database");
+				PreparedStatement preparedStatement = conn
+						.prepareStatement("insert into ACCOUNT(name,password) values (?, ? )");
+				// Parameters start with 1
+				preparedStatement.setString(1, name);
+				preparedStatement.setString(2, password);
 
-				Statement stm = conn.createStatement();
-				ResultSet rs0 = stm
-						.executeQuery("SELECT * FROM ACCOUNT WHERE name='"
-								+ name + "' AND password='" + password + "';");
+				preparedStatement.executeUpdate();
+				conn.close();
+				return "success";
 
-				if (rs0.next() == true) {
-					conn.close();
-					System.out.println("Disconnected from database");
-					Result = "Sorry!!! User name or Password is already taken";
-					setResult(Result);
-					return "failure";
-				} else {
-					String sql = "INSERT INTO ACCOUNT(name,password) "
-							+ "VALUES ('" + name + "', '" + password + "');";
-					stm.executeUpdate(sql);
-
-					conn.close();
-					Result = "Your account is created successfully";
-					setResult(Result);
-					return "success";
-				}
-
-			} catch (Exception e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
-				System.out.print("failed\n");
 			}
 
 		}
 
-		return "success";
-	}
-	
-	public String Update() {
-	
-			if(NewName.length()==0)
-			{
-				NewName = name;
-			}
-			if(Newpassword.length()==0)
-			{
-				Newpassword = password;
-			}
-			System.out.println("MySQL Connect Example.");
-			Connection conn = null;
-			String url = "jdbc:mysql://localhost:3306/";
-			String dbName = "USER";
-			String driver = "com.mysql.jdbc.Driver";
-			String userName = "root";
-			String password1 = "";
-			try {
-				Class.forName(driver).newInstance();
-				conn = DriverManager.getConnection(url + dbName, userName,
-						password1);
-				System.out.println("Connected to the database");
-
-				Statement stm = conn.createStatement();
-				ResultSet rs0 = stm
-						.executeQuery("SELECT * FROM ACCOUNT WHERE name='"
-								+ name + "' AND password='" + password + "';");
-
-				if (rs0.next() == true) {
-					
-					String sql = "UPDATE ACCOUNT SET name='"+NewName+"',password='"+Newpassword+"' WHERE name='"+name+"' AND password='"+password+"';";
-							
-					stm.executeUpdate(sql);
-					
-					conn.close();
-					System.out.println("Disconnected from database");
-					Result = "Account Updated";
-					setResult(Result);
-					return "success";
-				} else {
-					
-					conn.close();
-					Result = "Update Failed";
-					setResult(Result);
-					return "failure";
-				}
-
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.print("failed\n");
-			}
-
-		
-
-		return "success";
+		return "failure";
 	}
 
-	public String Delete() {
-		
-		if(name.equals(null))
-		{
+	public String Update() throws SQLException {
+
+		if (NewName.length() == 0) {
+			NewName = name;
+		}
+		if (Newpassword.length() == 0) {
+			Newpassword = password;
+		}
+		System.out.println("MySQL Connect Example.");
+		Connection conn = null;
+		conn = DBconnection(conn);
+		int id = 0;
+		try {
+
+			id = Select(conn, name, password);
+
+			PreparedStatement preparedStatement = conn
+					.prepareStatement("update ACCOUNT set name=?, password=? where id=?");
+			// Parameters start with 1
+			preparedStatement.setString(1, NewName);
+			preparedStatement.setString(2, Newpassword);
+			preparedStatement.setInt(3, id);
+			preparedStatement.executeUpdate();
+			conn.close();
+			return "success";
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "failure";
+
+	}
+
+	public String Delete() throws SQLException {
+
+		if (name.length() == 0) {
 			return "failure";
 		}
-		if(password.equals(null))
-		{
+		if (password.length() == 0) {
 			return "failure";
 		}
 		System.out.println("MySQL Connect Example.");
 		Connection conn = null;
-		String url = "jdbc:mysql://localhost:3306/";
-		String dbName = "USER";
-		String driver = "com.mysql.jdbc.Driver";
-		String userName = "root";
-		String password1 = "";
+		conn = DBconnection(conn);
+		int id = 0;
 		try {
-			Class.forName(driver).newInstance();
-			conn = DriverManager.getConnection(url + dbName, userName,
-					password1);
-			System.out.println("Connected to the database");
 
-			Statement stm = conn.createStatement();
-			ResultSet rs0 = stm
-					.executeQuery("SELECT * FROM ACCOUNT WHERE name='"
-							+ name + "' AND password='" + password + "';");
+			id = Select(conn, name, password);
 
-			if (rs0.next() == true) {
-				
-				String sql = "DELETE FROM ACCOUNT WHERE name='"+name+"' AND password='"+password+"';";
-						
-				stm.executeUpdate(sql);
-				
-				conn.close();
-				System.out.println("Disconnected from database");
-				return "success";
-			} else {
-				
-				conn.close();
-				Result = "Account delete action failed!!";
-				setResult(Result);
-				return "failure";
-			}
+			PreparedStatement preparedStatement = conn
+					.prepareStatement("delete from ACCOUNT where id=?");
+			// Parameters start with 1
 
-		} catch (Exception e) {
+			preparedStatement.setInt(1, id);
+
+			preparedStatement.executeUpdate();
+			conn.close();
+			System.out.println("MySQL Disconnected");
+			return "success";
+		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.print("failed\n");
 		}
 
-	
+		return "failure";
 
-	return "success";
-}
-	
-	
+	}
 
 	public String getPassword() {
 		return password;
